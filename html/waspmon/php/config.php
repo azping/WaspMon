ab959c322932b36d08271966dd54b6f590c90423
Pedro Marques <azping@users.noreply.github.com>
2017-05-02 12:55:40 +0100

    Upload of the webapp files

---
+++ html/waspmon/php/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'waspmon');
+   define('DB_PASSWORD', 'waspmon');
+   define('DB_DATABASE', 'waspmon');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
